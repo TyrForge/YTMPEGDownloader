46b45447e37c820ff8e9597e3d72dfe618ec431c
TyrForge <tyrforge.business@gmail.com>
2026-01-11 13:39:42 +0000

    Update BUILD EXE.bat

--- BUILD EXE.bat
+++ BUILD EXE.bat
@@ -3,6 +3,7 @@
 pip install -r requirements.txt
 pyinstaller --noconfirm --onefile --windowed --icon "./FILE.ico" --name "YT2MPEG"  "./main.py"
 rmdir /S /Q "./build/"
+DEL YT2MPEG.spec
 
 cls
 echo --------------DONE--------------
